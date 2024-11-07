9ddbe3cf3ab0b0e640c9fa55495974ff864ae854
bhartidubey1995 <bdubey@netlink.com>
2024-11-07 12:23:43 +0530

    Bharti Documents

---
+++ MoveFiels_Windows/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace MoveFiels_Windows
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
